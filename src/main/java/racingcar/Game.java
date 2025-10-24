1acded3a1b4bb4a4a06c0516ea06257d4d38579c
김충현 <winwiewie@gmail.com>
2025-10-24 18:46:06 +0900

    게임 경주장 구현

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,61 @@
+package racingcar;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Game {
+
+    private final List<Car> cars;
+    private final int lastTrack;
+
+    private final List<Car> winerCar = new ArrayList<>();
+
+    public Game(List<Car> cars, int lastTrack) {
+        this.cars = cars;
+        this.lastTrack = lastTrack;
+    }
+
+    public void gameStart() {gi
+        for (int i = 0; i < lastTrack; i++) {
+            car();
+        }
+    }
+
+    public void gameEnd() {
+        Car winer = cars.stream()
+                .max(Comparator.comparingInt(Car::getMoveCarCount))
+                .orElse(null);
+
+        for (Car car : cars) {
+            int moveCarCount = car.getMoveCarCount();
+
+            if (winer.getMoveCarCount() == moveCarCount) {
+                winerCar.add(car);
+            }
+        }
+
+        System.out.print("최종 우승자 : ");
+        String winersName = "";
+        for (int i = 0; i < winerCar.size(); i++) {
+            Car car = winerCar.get(i);
+            winersName = car.getCarName();
+
+            System.out.print(winersName);
+
+            if (i != (winerCar.size() -1)) {
+                System.out.print(", ");
+            }
+        }
+
+    }
+
+    private void car() {
+        for (Car car : cars) {
+            car.start();
+            car.carShow();
+        }
+        System.out.println();
+    }
+}
