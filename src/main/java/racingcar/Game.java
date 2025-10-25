4211b591079573f3f2fb5a869f50cd16a375921a
김충현 <winwiewie@gmail.com>
2025-10-25 17:58:51 +0900

    실수 지움

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -28,7 +28,7 @@ public class Game {
                 .max(Comparator.comparingInt(Car::getMoveCarCount))
                 .orElse(null);
 
-        if (winer == null) {ㅎ
+        if (winer == null) {
             System.out.println("잘못된 값을 입력 하셧습니다.");
             throw new IllegalArgumentException();
         }
