70fe56079fb51c68de078059f1ac0f03480f7b03
김충현 <winwiewie@gmail.com>
2025-10-27 20:53:07 +0900

    주석 추가

--- src/main/java/racingcar/CarNameException.java
+++ src/main/java/racingcar/CarNameException.java
@@ -11,6 +11,7 @@ public class CarNameException {
 
     private static final String DELIMITER = ",";
 
+    // null 예외
     private static void carNameNull(String strInput) {
         if (strInput == null) {
             System.out.println("잘못된 값을 입력 하셨습니다.");
@@ -18,6 +19,7 @@ public class CarNameException {
         }
     }
 
+    // 입력시 아무것도 입력을 안햇을때 생기는 예외
     private static void carNameEmpty(String strInput) {
         strInput = strInput.trim();
         if (strInput.isEmpty()) {
@@ -26,6 +28,7 @@ public class CarNameException {
         }
     }
 
+    // 특수 문자 및 숫자를 입력시 예외(","제외 한글 입력 가능)
     private static void carNameFalse(String strInput) {
         String trueStr = "^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣,]*$";
         if (!strInput.matches(trueStr)) {
@@ -34,6 +37,7 @@ public class CarNameException {
         }
     }
 
+    // 처음에 , 입력시 발생 하는 예외
     private static  void carNameStartFalse(String strInput) {
         if (strInput.startsWith(DELIMITER)){
             System.out.println("처음 글자로 ',' 입력이 불가능 합니다.");
@@ -41,6 +45,7 @@ public class CarNameException {
         }
     }
 
+    // 마지막 글자에 , 입력시 발생 하는 예외
     private static void carNameEndFalse(String strInput) {
         String lastStrInput = strInput.substring(strInput.length()-1);
         if (lastStrInput.equals(DELIMITER)) {
@@ -49,6 +54,7 @@ public class CarNameException {
         }
     }
 
+    // 자동차 3개 이상 입력후 중간에 자동차 이름이 공백일시 발생하는 예외
     private static void carNameTrimFalse(String strInput) {
         String[] strCar = strInput.split(DELIMITER);
 
@@ -61,7 +67,7 @@ public class CarNameException {
         }
     }
 
-
+    // 이름 길이가 6글자 이상일때 발생하는 예외
     private static void carNameLengthFalse(String strInput) {
         String[] strCar = strInput.split(DELIMITER);
         for (String strName : strCar) {
@@ -72,6 +78,7 @@ public class CarNameException {
         }
     }
 
+    // 똑같은 이름이 2개이상일 경우 발생하는 예외
     private static void carNameDuplicate(String strInput) {
         String[] strCar = strInput.split(DELIMITER);
         List<String> list = Arrays.asList(strCar);
--- src/main/java/racingcar/GameTrackException.java
+++ src/main/java/racingcar/GameTrackException.java
@@ -5,6 +5,7 @@ public class GameTrackException {
     private GameTrackException() {
     }
 
+    //null 예외
     private static void gameTrackNull(String countInput) {
         if (countInput == null) {
             System.out.println("잘못된 값을 입력 하셨습니다.");
@@ -12,6 +13,7 @@ public class GameTrackException {
         }
     }
 
+    // 입력시 아무것도 입력을 안햇을때 생기는 예외
     private static void gameTrackCountEmpty(String countInput) {
         countInput = countInput.trim();
         if (countInput.isEmpty()) {
@@ -20,6 +22,7 @@ public class GameTrackException {
         }
     }
 
+    // 숫자가 아닌 다른 값을 입력 했을때 예외
     private static void gameTrackCountFalse(String countInput) {
         String trueStr = "^[0-9]*$";
         if (!countInput.matches(trueStr)) {
@@ -28,6 +31,7 @@ public class GameTrackException {
         }
     }
 
+    // 처음에 0을 입력 했을때 예외
     private static  void gameTrackStartFalse(String countInput) {
         if (countInput.startsWith("0")){
             System.out.println("처음 에는 '0' 입력이 불가능 합니다.");
